ca018fb98574b17460a7b450ca38d87c0ebad795
shyu216 <sihong1@student.unimelb.edu.au>
2024-07-31 15:41:08 +1000

    change to theme hope

--- docs/.vuepress/.temp/prismjs/config.js
+++
@@ -1,3 +0,0 @@
-import "/Users/yusihong/Desktop/knownoevil/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
-import "/Users/yusihong/Desktop/knownoevil/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
-import "/Users/yusihong/Desktop/knownoevil/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
